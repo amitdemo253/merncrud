0222e3e0e98b19e2ad5cfd29f1251c329058f974
Amit  Jaiswal <amitdemo253@gmail.com>
2022-07-28 19:37:34 +0530

    route change

--- routes/router.js
+++ routes/router.js
@@ -17,27 +17,30 @@ router.post("/register",async(req,res)=>{
     if(!name || !email || !age || !mobile || !work || !add || !desc){
         res.status(422).json("plz fill the data");
     }
+    else{
 
-    try {
+        try {
         
-        const preuser = await users.findOne({email:email});
-        console.log(preuser);
-
-        if(preuser){
-            res.status(422).json("this is user is already present");
-        }else{
-            const adduser = new users({
-                name,email,age,mobile,work,add,desc
-            });
-
-            await adduser.save();
-            res.status(201).json(adduser);
-            console.log(adduser);
+            const preuser = await users.findOne({email:email});
+            console.log(preuser);
+    
+            if(preuser){
+                res.status(422).json("this is user is already present");
+            }else{
+                const adduser = new users({
+                    name,email,age,mobile,work,add,desc
+                });
+    
+                await adduser.save();
+                res.status(201).json(adduser);
+                console.log(adduser);
+            }
+    
+        } catch (error) {
+            res.status(422).json(error);
         }
-
-    } catch (error) {
-        res.status(422).json(error);
     }
+
 })
 
 
