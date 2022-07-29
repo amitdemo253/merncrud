72539907b7575d2a89d11de78e6f7290be14cf0f
Amit  Jaiswal <amitdemo253@gmail.com>
2022-07-29 12:30:58 +0530

    validation

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,34 +1,40 @@
 const mongoose = require("mongoose");
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true,'Please enter name'],
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true,'Please enter an email'],
+        unique: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     age: {
         type: String,
-        required: true
+        required: [true,'Please enter age'],
     },
     mobile: {
         type: Number,
-        required: true
+        required: [true,'Please enter mobile number'],
     },
     work: {
         type: String,
-        required: true
+        required: [true,'Please enter work detail'],
     },
     add: {
         type: String,
-        required: true
+        required: [true,'Please enter an address'],
     },
     desc: {
         type: String,
-        required: true
+        required: [true,'Please enter description'],
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
     }
 });
 
